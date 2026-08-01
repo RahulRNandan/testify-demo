f64927b3a473d6d6bd0d6725e4817c6a80975387
vvdn <rahul.nandan@vvdntech.in>
2026-08-01 18:11:29 +0530

    latest submission

---
+++ test_cart.py
@@ -0,0 +1,26 @@
+from testify import TestCase, assert_equal, setup, teardown
+
+class ShoppingCart:
+    def __init__(self):
+        self.items = []
+    def add_item(self, item: str):
+        self.items.append(item)
+    def clear(self):
+        self.items = []
+    
+class ShoppingCartTest(TestCase):
+    @setup
+    def prepare_cart(self):
+        self.cart.add_item("Laptop")
+
+    @teardown
+    def prepare_cart(self):
+        self.cart.clear()
+    def test_initial_item_count():
+        assert_equal(len(self.cart.items), 1)
+    
+    def test_adding_second_item(self):
+        self.cart.add_item("Mouse")
+        assert_equal(len(self.cart.items), 2)
+
+        
